02f271631158def9bd3b7db655e9155fe4e8e401
wertyyy12 <wertyyyy102@gmail.com>
2024-09-01 13:32:58 -0500

    5 dimensional results encouraging

--- rotor_v2.py
+++ rotor_v2.py
@@ -13,14 +13,11 @@ import corner
 # !pip install -U emcee
 # import emcee
 import tensorflow as tf
-import tensorflow.compat.v2 as tf
+# import tensorflow.compat.v2 as tf
 from time_logger import start_logger
-tf.enable_v2_behavior()
+# tf.enable_v2_behavior()
 
 import tensorflow_probability as tfp
-import inflect
-import os
-num_to_words = inflect.engine().number_to_words
 
 # sns.reset_defaults()
 # sns.set_context(context='talk',font_scale=0.7)
@@ -46,9 +43,9 @@ stop_logger = start_logger("alive", interval=30)
 SEED = 42229
 simple_mode = True
 OUTPUT_DIM = 4 if not simple_mode else 1
-NUM_PARAMETERS = 23 if not simple_mode else 2
+NUM_PARAMETERS = 23 if not simple_mode else 5
 MEASUREMENT_ERROR = tfc([4., 5., 3., 2.]) if not simple_mode else tfc([4.]) # shape = (OUTPUT_DIM,)
-number_of_theta_values = 1000
+number_of_theta_values = 20000
 # print(tfc([0., 1., .2]).shape)
 THETA_VALUES = tf.cast(tf.reshape(tf.linspace(-5., 5., number_of_theta_values), (number_of_theta_values,)), dtype) # shape = (number_of_theta_values,)
 # print(THETA_VALUES.shape)
@@ -60,13 +57,13 @@ if eager:
   print("EAGER mode turned on")
 true_parameters = tf.cast(tf.linspace(1, 10, NUM_PARAMETERS), dtype)[tf.newaxis, ...] # shape = (1, NUM_PARAMETERS) 
 print("true_parameters = ", true_parameters)
-num_samples_string = "1e4"
+num_samples_string = "2e4"
 num_posterior_samples = int(num_samples_string.split("e")[0]) * (10**int(num_samples_string.split("e")[1]))
 print("num_posterior_samples = ", num_posterior_samples)
-run_identifier = f"{num_samples_string}_prior_{NUM_PARAMETERS}param"
-num_temperatures = 10 
+run_identifier = f"{num_samples_string}_original"
+num_temperatures = 10
 inverse_temperatures = 0.6**tf.range(num_temperatures, dtype=dtype)
-num_burn_in_steps = 5000 
+num_burn_in_steps = 10000
 parameter_labels = [chr(x) for x in range(ord('A'), ord('A') + NUM_PARAMETERS)]
 step_size = tf.reshape(tf.repeat(tf.constant(.5, dtype=dtype), num_temperatures * NUM_PARAMETERS), (num_temperatures, NUM_PARAMETERS))
 initial_state = tf.zeros(NUM_PARAMETERS, dtype=dtype)
@@ -104,32 +101,32 @@ else:
 thetas = THETA_VALUES
 ys = model(thetas, true_parameters)
 
+# @tf.function
+# def lnprior(parameters):
+# #  print("parameters.shape", parameters.shape) 
+# #  print(parameters)
+#   # Check if all elements are greater than 0
+#   all_greater_than_zero = tf.reduce_all(parameters >= 1, axis=1)
+# 
+#   # Check if all elements are less than 10
+#   all_less_than_ten = tf.reduce_all(parameters <= 10, axis=1)
+# 
+#   # Check if all conditions are met
+#   not_valid_parameters = tf.cast(tf.logical_not(tf.logical_and(all_greater_than_zero, all_less_than_ten)), dtype)
+# 
+#   neg_inf = tf.constant([-tf.float32.max])
+#   # Return 0.0 if valid, -infinity otherwise
+# #  print("valid_parameters.shape", valid_parameters.shape)
+# #  print(valid_parameters)
+# #  print("neg_inf.shape", neg_inf.shape)
+# #  print(neg_inf)
+# #  print("final result shape", tf.multiply(valid_parameters, neg_inf).shape)
+# #  print(tf.multiply(valid_parameters, neg_inf))
+# 
+#   return tf.multiply(not_valid_parameters, neg_inf)
+
 @tf.function
 def lnprior(parameters):
-#  print("parameters.shape", parameters.shape) 
-#  print(parameters)
-  # Check if all elements are greater than 0
-  all_greater_than_zero = tf.reduce_all(parameters >= 1, axis=1)
-
-  # Check if all elements are less than 10
-  all_less_than_ten = tf.reduce_all(parameters <= 10, axis=1)
-
-  # Check if all conditions are met
-  not_valid_parameters = tf.cast(tf.logical_not(tf.logical_and(all_greater_than_zero, all_less_than_ten)), dtype)
-
-  neg_inf = tf.constant([-tf.float32.max])
-  # Return 0.0 if valid, -infinity otherwise
-#  print("valid_parameters.shape", valid_parameters.shape)
-#  print(valid_parameters)
-#  print("neg_inf.shape", neg_inf.shape)
-#  print(neg_inf)
-#  print("final result shape", tf.multiply(valid_parameters, neg_inf).shape)
-#  print(tf.multiply(valid_parameters, neg_inf))
-
-  return tf.multiply(not_valid_parameters, neg_inf)
-
-@tf.function
-def lnprior_old(parameters):
   return tf.constant(0.0, dtype=dtype)
 
 @tf.function
@@ -146,13 +143,26 @@ def unnormalized_posterior(parameters):
 #    step_size=step_size,
 #    num_leapfrog_steps=2)
 
+#def make_kernel_fn(target_log_prob_fn):
+# kernel = tfp.mcmc.HamiltonianMonteCarlo(
+#   target_log_prob_fn=target_log_prob_fn,
+#   step_size=step_size,
+#   num_leapfrog_steps=2)
+# return tfp.mcmc.SimpleStepSizeAdaptation(
+#       kernel,
+#       num_adaptation_steps=int(.8 * num_burn_in_steps),
+#       target_accept_prob=np.float64(.3))
+
 def make_kernel_fn(target_log_prob_fn):
  kernel = tfp.mcmc.HamiltonianMonteCarlo(
    target_log_prob_fn=target_log_prob_fn,
    step_size=step_size,
    num_leapfrog_steps=2)
  return tfp.mcmc.SimpleStepSizeAdaptation(
-       kernel,
+       tfp.mcmc.TransformedTransitionKernel(
+        inner_kernel=kernel, 
+        bijector=tfb.Identity()
+       ),
        num_adaptation_steps=int(.8 * num_burn_in_steps),
        target_accept_prob=np.float64(.3))
 
@@ -183,51 +193,6 @@ def run_chain(kernel, initial_state, num_posterior_samples=num_posterior_samples
     trace_fn=lambda current_state, kernel_results: kernel_results
 )
 
-@tf.function
-def run_chain_block(kernel, initial_state, num_posterior_samples=num_posterior_samples, num_burnin_steps=num_burn_in_steps):
-  # print(kernel)
-  current_state = initial_state
-  kernel_results = kernel.bootstrap_results(current_state)
-  chain_blocks = []
-  trace_blocks = []
-  # current_state = initial_state
-
-  
-  num_blocks = 3
-  for i in range(num_blocks):
-    print(f"running block {i}")
-    chain, trace, kernel_results = tfp.mcmc.sample_chain(
-      num_posterior_samples,
-      num_burnin_steps=num_burnin_steps,
-      current_state=current_state,
-      kernel=kernel,
-      trace_fn=lambda current_state, kernel_results: kernel_results,
-      previous_kernel_results=kernel_results,
-      return_final_kernel_results=True,
-    )
-    # print("chain ended at ", chain[-1, :])
-    # print("full chain was ", chain)
-    # print(tf.nest.map_structure(lambda x: x[-1], chain)[0])
-    current_state = chain[-1, :]
-    chain_blocks.append(chain)
-    trace_blocks.append(trace)
- 
-
-   #  with open("mcmc_inter_kernel", "wb") as f:
-   #    pickle.dump(kernel_results, f)
-    # print(chain[-1, :])
-    # print(trace)
-    # print(kernel_results)
-
-	# Do your partial analysis here.
-
-
-  full_chain = tf.nest.map_structure(lambda *parts: tf.concat(parts, axis=0), *chain_blocks)
-  full_trace = tf.nest.map_structure(lambda *parts: tf.concat(parts, axis=0), *trace_blocks)
-  print("chain completed")
-  return full_chain
-  # return full_chain, full_trace
-
 remc = tfp.mcmc.ReplicaExchangeMC(
     target_log_prob_fn=unnormalized_posterior,
     inverse_temperatures=inverse_temperatures,
@@ -246,16 +211,13 @@ tf.io.write_file("./saved_mcmc/mcmc_saved_chain", one_string)
 print("results were", results)
 print(f"saved to file ./saved_mcmc/mcmc_saved_chain")
 """	
+
 try:
     print("plotting corner")
     fig = corner.corner(samples.numpy(),show_titles=True,labels=parameter_labels,plot_datapoints=True,quantiles=[0.16, 0.5, 0.84], truths=true_parameters.numpy()[0])
     print("corner created")
-    directory = num_to_words(NUM_PARAMETERS)
-    saved_path = os.path.join(directory, f"{run_identifier}_corner_plot.png")
-    if not os.path.isdir(directory):
-      os.mkdir(directory)
-    fig.savefig(saved_path, dpi=300, bbox_inches="tight")
-    print(f"corner saved to {saved_path}")
+    fig.savefig(f"{run_identifier}_corner_plot.png", dpi=300, bbox_inches="tight")
+    print(f"corner saved to {run_identifier}_corner_plot.png")
     print("samples.shape", samples.numpy().shape)
     for i in range(NUM_PARAMETERS):
       plt.figure()
@@ -281,5 +243,7 @@ try:
 except Exception as e:
     print("an error occurred when plotting:")
     print(e) 
+    print("the samples were")
+    print(samples)
 
 stop_logger()
